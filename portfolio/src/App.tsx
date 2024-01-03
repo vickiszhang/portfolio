86c18f1ba280d682b3ede632921c2a19d047a8be
Vicki <s6p1j@students.cs.ubc.ca>
2024-01-03 15:25:21 -0800

    changes

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      home
+      <div>
+
+        <img width="55px" src='tian.png'></img>
+        <img width="50px" src='hui.png'></img>
+
+      </div>
     </>
   )
 }
